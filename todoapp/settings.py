93c35ffa03946aa3e06c09e53e1e91472ddb0199
scrappy <scrappycoco902@gmail.com>
2024-10-26 18:14:37 +0530

    small

--- todoapp/settings.py
+++ todoapp/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-f11why!r)x$j-*h3-f-gq9$686tjvc%+cyiu&$2g%-y))3a5r+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -90,6 +91,11 @@ DATABASES = {
     }
 }
 
+heroku_db = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(heroku_db)
+
+
+
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
@@ -124,7 +130,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
